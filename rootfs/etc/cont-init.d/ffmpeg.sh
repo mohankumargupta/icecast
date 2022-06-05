09bf88513cfb9849b776a8016076ef53f454adab
Mohan Gupta <mohan.kumar.gupta@gmail.com>
2022-06-05 06:12:06 +0000

    introduced configuration

--- rootfs/etc/cont-init.d/ffmpeg.sh
+++ rootfs/etc/cont-init.d/ffmpeg.sh
@@ -1,15 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-contenv bashio
 
-mkdir -p /etc/services.d/ffmpeg_stream_name
-echo '#!/usr/bin/env bash' > /etc/services.d/ffmpeg_stream_name/run
-echo 'while true' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'do' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'if [ -f /var/log/icecast2/error.log ];' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'then' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'echo moomooomoo' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'break' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'fi' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'done' >> /etc/services.d/ffmpeg_stream_name/run
-echo 'ffmpeg -re -i https://mirchiplaylive.akamaized.net/hls/live/2036929/MUM/MRETRO_Auto.m3u8 -c:a libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://source:password@localhost:8000/mirchi' >> /etc/services.d/ffmpeg_stream_name/run
+#cat /data/options.json
 
-chmod +x /etc/services.d/ffmpeg_stream_name/run
+for stream in $(bashio::config "streams")
+do
+ stream_name=$(bashio::jq "$stream" ".name")
+ stream_url=$(bashio::jq "$stream" ".stream_url")
+ #echo "Stream name: $stream_name"
+ #echo "Stream URL: $stream_url"
+ #echo "----"
+ mkdir -p /etc/services.d/ffmpeg_${stream_name}
+ bashio::var.json \
+   STREAM_NAME $stream_name \
+   STREAM_URL $stream_url | tempio \
+                            -template /usr/share/tempio/ffmpeg.gtpl \
+                            -out /etc/services.d/ffmpeg_${stream_name}/run
+ chmod +x /etc/services.d/ffmpeg_${stream_name}/run
+ cat /etc/services.d/ffmpeg_${stream_name}/run
+ #echo "----"
+done
---
+++ rootfs/usr/share/tempio/ffmpeg.gtpl
@@ -0,0 +1,4 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::net::wait_for 8000
+ffmpeg -re -i {{ .STREAM_URL }} -c:a libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://source:password@localhost:8000/{{ .STREAM_NAME }}
