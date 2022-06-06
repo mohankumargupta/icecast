e867f4f97541bcd8fe47811b7b32904b0b291f49
Mohan Gupta <mohan.kumar.gupta@gmail.com>
2022-06-06 07:17:17 +0000

    handle whitespace in addon configuration properly

--- rootfs/etc/cont-init.d/ffmpeg.sh
+++ rootfs/etc/cont-init.d/ffmpeg.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/with-contenv bashio
 
-#cat /data/options.json
-
-for stream in $(bashio::config "streams")
+for stream in $(bashio::config 'streams|keys')
 do
- stream_name=$(bashio::jq "$stream" ".name")
- stream_url=$(bashio::jq "$stream" ".stream_url")
- #echo "Stream name: $stream_name"
- #echo "Stream URL: $stream_url"
- #echo "----"
- mkdir -p /etc/services.d/ffmpeg_${stream_name}
+ stream_name=$(bashio::config "streams[${stream}].name")
+ stream_url=$(bashio::config "streams[${stream}].stream_url")
+ mountpoint=$(bashio::config "streams[${stream}].mountpoint")
+
+ mkdir -p /etc/services.d/ffmpeg_${mountpoint}
  bashio::var.json \
-   STREAM_NAME $stream_name \
-   STREAM_URL $stream_url | tempio \
+   STREAM_NAME "$stream_name" \
+   STREAM_URL "$stream_url"   \
+   MOUNTPOINT "$mountpoint" | tempio \
                             -template /usr/share/tempio/ffmpeg.gtpl \
-                            -out /etc/services.d/ffmpeg_${stream_name}/run
- chmod +x /etc/services.d/ffmpeg_${stream_name}/run
- #cat /etc/services.d/ffmpeg_${stream_name}/run
- #echo "----"
+                            -out /etc/services.d/ffmpeg_${mountpoint}/run
+ chmod +x /etc/services.d/ffmpeg_${mountpoint}/run
 done
